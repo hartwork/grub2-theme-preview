f82650df0514eb859b455ce6831e81f6a129a1e8
Sebastian Pipping <sebastian@pipping.org>
2017-12-29 20:04:38 +0100

    Add parameter --plain-rescue-image
    to help debugging issues like #14.

--- grub2_theme_preview/cli.py
+++ grub2_theme_preview/cli.py
@@ -197,7 +197,6 @@ def parse_command_line():
     parser.add_argument('--image', action='store_true', help='Preview a background image rather than a whole theme')
     parser.add_argument('--grub-cfg', metavar='PATH', help='Path of custom grub.cfg file to use (default: /boot/grub{2,}/grub.cfg)')
     parser.add_argument('--verbose', default=False, action='store_true', help='Increase verbosity')
-    parser.add_argument('--debug', default=False, action='store_true', help='Enable debugging output')
     parser.add_argument('--resolution', metavar='WxH', type=resolution, help='Set a custom resolution, e.g. 800x600')
     parser.add_argument('--timeout', metavar='SECONDS', dest='timeout_seconds', type=timeout, default=30,
             help='Set timeout in whole seconds or -1 to disable (default: %(default)s seconds)')
@@ -208,6 +207,12 @@ def parse_command_line():
     commands.add_argument('--grub2-mkrescue', default='grub-mkrescue', metavar='COMMAND', help='grub2-mkrescue command (default: %(default)s)')
     commands.add_argument('--qemu', metavar='COMMAND', help='KVM/QEMU command (default: qemu-system-<machine>)')
     commands.add_argument('--xorriso', default='xorriso', metavar='COMMAND', help='xorriso command (default: %(default)s)')
+
+    debugging = parser.add_argument_group('debugging arguments')
+    debugging.add_argument('--debug', default=False, action='store_true', help='Enable debugging output')
+    debugging.add_argument('--plain-rescue-image', default=False, action='store_true',
+                           help='Use unprocessed GRUB rescue image with no theme patched in; '
+                           'useful for checking if a plain GRUB rescue image shows up a GRUB shell, successfully.')
 
     options = parser.parse_args()
 
@@ -266,8 +271,11 @@ def _inner_main(options):
                 options.grub2_mkrescue,
                 '--xorriso', options.xorriso,
                 '--output', abs_tmp_img_file,
-                'boot/grub/grub.cfg=%s' % abs_tmp_grub_cfg_file,
                 ]
+
+            if not options.plain_rescue_image:
+                assemble_cmd.append('boot/grub/grub.cfg=%s' % abs_tmp_grub_cfg_file)
+
             try:
                 if options.image:
                     assemble_cmd += [
