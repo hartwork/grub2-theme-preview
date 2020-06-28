5c889103916bf789612e79c100f4024c294ac502
Sebastian Pipping <sebastian@pipping.org>
2020-06-29 00:40:51 +0200

    Fix OVMF image location for Arch

--- grub2_theme_preview/__main__.py
+++ grub2_theme_preview/__main__.py
@@ -254,6 +254,7 @@ def _grub2_ovmf_tuple():
     """
     _DEBIAN = 'Debian'
     distro_map = {
+        'Arch Linux': ('/usr/share/edk2-ovmf/x64/OVMF_CODE.fd', 'edk2-ovmf'),
         _DEBIAN: ('/usr/share/OVMF/OVMF_CODE.fd', 'ovmf'),
         'Gentoo': ('/usr/share/edk2-ovmf/OVMF_CODE.fd', 'sys-firmware/edk2-ovmf'),
     }
