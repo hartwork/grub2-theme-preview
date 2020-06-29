fb7acca58d4de8e9a085ca7f9ff13f83b02e74fa
Sebastian Pipping <sebastian@pipping.org>
2020-06-29 20:36:36 +0200

    Bump version to 1.5.0

--- grub2_theme_preview/version.py
+++ grub2_theme_preview/version.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2015 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under GPL v2 or later
 
-_VERSION = (1, 4, 4)
+_VERSION = (1, 5, 0)
 VERSION_STR = '.'.join([str(e) for e in _VERSION])
