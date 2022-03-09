da3992eacccaf6e665d7084b54c63dd6a700dcb4
Sebastian Pipping <sebastian@pipping.org>
2022-03-09 22:13:07 +0100

    pre-commit: Help yapf with redability

--- grub2_theme_preview/__main__.py
+++ grub2_theme_preview/__main__.py
@@ -260,22 +260,22 @@ def parse_command_line():
                         metavar='WxH',
                         type=resolution,
                         help='set a custom resolution, e.g. 800x600')
-    parser.add_argument(
-        '--timeout',
-        metavar='SECONDS',
-        dest='timeout_seconds',
-        type=timeout,
-        default=30,
-        help='set GRUB timeout in whole seconds or -1 to disable (default: %(default)s seconds)')
-    parser.add_argument(
-        '--add',
-        default=[],
-        action='append',
-        dest='addition_requests',
-        metavar='TARGET=/SOURCE',
-        type=validate_grub2_mkrescue_addition,
-        help=('make grub2-mkrescue add file(s) from /SOURCE to /TARGET in the rescue image'
-              ' (can be passed multiple times)'))
+    parser.add_argument('--timeout',
+                        metavar='SECONDS',
+                        dest='timeout_seconds',
+                        type=timeout,
+                        default=30,
+                        help='set GRUB timeout in whole seconds or -1 to disable'
+                        ' (default: %(default)s seconds)')
+    parser.add_argument('--add',
+                        default=[],
+                        action='append',
+                        dest='addition_requests',
+                        metavar='TARGET=/SOURCE',
+                        type=validate_grub2_mkrescue_addition,
+                        help=('make grub2-mkrescue add file(s) from /SOURCE to /TARGET'
+                              ' in the rescue image'
+                              ' (can be passed multiple times)'))
     parser.add_argument('source',
                         metavar='PATH',
                         help='path of theme directory (or PNG/TGA image file) to preview')
@@ -294,25 +294,25 @@ def parse_command_line():
                           help='xorriso command (default: %(default)s)')
 
     qemu = parser.add_argument_group('arguments related to invokation of QEMU/KVM')
-    qemu.add_argument(
-        '--no-kvm',
-        dest='enable_kvm',
-        default=True,
-        action='store_false',
-        help='do not pass -enable-kvm to QEMU'
-        ' (and hence fall back to acceleration "tcg" which is significantly slower than KVM)')
+    qemu.add_argument('--no-kvm',
+                      dest='enable_kvm',
+                      default=True,
+                      action='store_false',
+                      help='do not pass -enable-kvm to QEMU'
+                      ' (and hence fall back to acceleration "tcg"'
+                      ' which is significantly slower than KVM)')
 
     debugging = parser.add_argument_group('debugging arguments')
     debugging.add_argument('--debug',
                            default=False,
                            action='store_true',
                            help='enable debugging output')
-    debugging.add_argument(
-        '--plain-rescue-image',
-        default=False,
-        action='store_true',
-        help='use unprocessed GRUB rescue image with no theme patched in; '
-        'useful for checking if a plain GRUB rescue image shows up a GRUB shell, successfully.')
+    debugging.add_argument('--plain-rescue-image',
+                           default=False,
+                           action='store_true',
+                           help='use unprocessed GRUB rescue image with no theme patched in; '
+                           'useful for checking if a plain GRUB rescue image'
+                           ' shows up a GRUB shell, successfully.')
 
     options = parser.parse_args()
 
